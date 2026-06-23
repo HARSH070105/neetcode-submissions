6022c998d0cb1090b9136699b03cbc7e736e081e
Harsh Kapoor <128182699+HARSH070105@users.noreply.github.com>
2026-06-23 15:46:28 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,38 @@
+class MinStack {
+public:
+    stack<int> st;
+    stack<int> min;
+    
+    MinStack() {
+
+    }
+    
+    void push(int val) {
+        if(st.empty()){
+            st.push(val);
+            min.push(val);
+        }
+        else{
+            if (val<=min.top()){
+                min.push(val);
+            }
+            else{
+                min.push(min.top());
+            }
+            st.push(val);
+        }
+    }
+    
+    void pop() {
+        st.pop();
+        min.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return min.top();        
+    }
+};
